6cd1a9bfe2ef516c1e3e9e03495c4cc09c4f02bf
Burak Avar <burakavar@gmail.com>
2023-04-23 22:36:32 +0300

    settings scene edited

--- Assets/Scripts/SettingsControl.cs
+++ Assets/Scripts/SettingsControl.cs
@@ -1,12 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class SettingsControl : MonoBehaviour
 {
+   [SerializeField] private Button easyButton, mediumButton, hardButton;
+   
    public void MainMenu()
    {
       SceneManager.LoadScene("Menu");
+   }
+
+   private void Start()
+   {
+      easyButton.onClick.AddListener((() =>
+      {
+         OptionSelected(easyButton.GetComponentInChildren<Text>().text);
+      }));
+      
+      mediumButton.onClick.AddListener((() =>
+      {
+         OptionSelected(mediumButton.GetComponentInChildren<Text>().text);
+      }));
+      
+      hardButton.onClick.AddListener((() =>
+      {
+         OptionSelected(hardButton.GetComponentInChildren<Text>().text);
+      }));
+   }
+
+   public void OptionSelected(string _level)
+   {
+      switch (_level)
+      {
+         case "EASY":
+            easyButton.interactable = false;
+            mediumButton.interactable = true;
+            hardButton.interactable = true;
+            break;
+         case "MEDIUM":
+            mediumButton.interactable = false;
+            easyButton.interactable = true;
+            hardButton.interactable = true;
+            break;
+         case "HARD":
+            hardButton.interactable = false;
+            easyButton.interactable = true;
+            mediumButton.interactable = true;
+            break;
+         default:
+            break;
+      }
    }
 }
